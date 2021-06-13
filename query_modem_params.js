058d6e31fb1f5a854be1ec60dde97d818191a8af
Jason Hobbs <telnetdoogie@gmail.com>
2021-06-13 04:31:32 -0500

    Update query_modem_params.js

--- query_modem_params.js
+++ query_modem_params.js
@@ -1,5 +1,5 @@
 //Enter this into the console of the browser when on the modem's Admin UI.
-//(this script adapted from tifan's blog: https://tifan.net/blog/2021/04/01/enable-bridge-mode-on-verizon-5g-home-router-lv55-lvskisp/
+//(this script adapted from tifan's blog: https://tifan.net/blog/2021/04/01/enable-bridge-mode-on-verizon-5g-home-router-lv55-lvskisp/)
 
 function readParams(objectName){
   WNC.DM.get({
