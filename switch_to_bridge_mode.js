0d26d054bd7fe1418cd8a2cda68c917d8b920200
Jason Hobbs <telnetdoogie@gmail.com>
2021-06-20 02:44:14 -0500

    Update switch_to_bridge_mode.js

--- switch_to_bridge_mode.js
+++ switch_to_bridge_mode.js
@@ -1,6 +1,6 @@
 //Copy this into the browser's console on the Home page of the UI after logging in, and run it to switch the modem to bridge mode.
 
-$("#restart-modal .modal-body").html("<p>While your Device is restarting, its Wi-Fi network won't be available to the devices connected to it, and you won't be able to control your smart home devices. To access the Admin UI after the restart, try \'http://{public_ip+1 or public_ip-1}/cgi-bin/luci\'</p>");
+$("#restart-modal .modal-body").html("<p>While your Device is restarting, its Wi-Fi network won't be available to the devices connected to it, and you won't be able to control your smart home devices. To access the Admin UI after the restart, try \'http://{public_ip+1 or public_ip-1}/cgi-bin/luci/verizon/system_settings\'</p>");
 var msg = {
   title: "<h4 class=\"modal-title\"><b>System Restarting in Bridge Mode...</b></h4>",
   context: "<br><p>Please wait for a few minutes.</p>",
