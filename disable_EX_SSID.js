27461105c9c12dfe9cb4f0234afaf463dc8e501e
Jason Hobbs <telnetdoogie@gmail.com>
2021-12-15 21:41:33 -0600

    Create disable_EX_SSID.js

---
+++ disable_EX_SSID.js
@@ -0,0 +1,28 @@
+//you should make sure this is the radio the EX SSID is running on first by digging through:
+function readParams(objectName){
+  WNC.DM.get({
+      args: {
+          Object: objectName
+      },
+    success: function s(objs, status) {
+      console.log("success: ", status, objs)
+    },
+    error: function s(objs, status) {
+      console.log("error: ", status, objs)
+    },
+  })
+}
+
+//then once you've confirmed, use the below with the correct radio number.
+WNC.DM.set({
+  args: {
+    Object:"Device.WiFi.Radio.4.",
+    "Enable":false
+  },
+  success: function s(objs, status) {
+    console.log("success: ", status, objs)
+  },
+  error: function s(objs, status) {
+    console.log("error: ", status, objs)
+  },
+})
